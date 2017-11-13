47397f4ef6e67e148acf0557746cf0e4b9b3d152
Sheng0o0 <shenghaozheng@mybihs.org>
2017-11-13 22:09:55 +0000

    done something

--- db-search-functions.js
+++ db-search-functions.js
@@ -10,25 +10,49 @@ function getVersion(handleVersion) {
     // the database snapshot and passes it as an argument to the 'handleVersion'
     // callback.
     
-    //var version = snapshot.val();
-    //handleVersion(version);
+    var version = snapshot.val();
+    handleVersion(version);
   });
 }
 
 function getHelloWorldValue(handleType) {
   // TODO: Your code goes here. Hint: use 'getVersion' as a template.
+  var database = firebase.database();
+  var databaseTypeReference = database.ref('/db-search/hello-world');
+  databaseTypeReference.once('value').then(function(snapshot) {
+    var version = snapshot.val();
+    handleType(version);
+  });
 }
 
 function applyDiscount(handleNewPrice) {
   // TODO: Your code goes here.
+  var database = firebase.database();
+  var databaseNewPriceReference = database.ref('/db-search/discount');
+  databaseNewPriceReference.once('value').then(function(snapshot) {
+    var version = snapshot.val()*100;
+    handleNewPrice(version);
+  });
 }
 
 function getArrayLength(handleLength) {
   // TODO: Your code goes here.
+  var database = firebase.database();
+  var databaseHandleLengthReference = database.ref('/db-search/array');
+  databaseHandleLengthReference.once('value').then(function(snapshot) {
+    var version = snapshot.val().length;
+    handleLength(version);
+  });
 }
 
 function getLastArrayElement(handleElement) {
   // TODO: Your code goes here.
+  var database = firebase.database();
+  var databaseLastArrayReference = database.ref('/db-search/array');
+  databaseLastArrayReference.once('value').then(function(snapshot) {
+    var version = snapshot.val().lastIndexOf(char);
+    handleElement(version);
+  });
 }
 
 function getArrayJoin(handleJoin) {
